8677d30f88148db04f850cc0c8e40902770947b5
Ezza123 <p.balkowski123@gmail.com>
2022-11-24 09:59:22 +0100

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let arrayToReverse = text.split('');
+  let reversedArray = arrayToReverse.reverse();
+  let string = reversedArray.join('');
+  
+  return string 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
