6de56a9888ee34a593431f762dbcabbb5e40a655
Ezza123 <p.balkowski123@gmail.com>
2022-11-25 19:22:08 +0100

    ;(

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
 
-};
+  const removeFromArray = function (...arg) {
+    let array = arg[0];
+    let newArray = [];
+  
+    array.forEach((item) => {
+      if (!arg.includes(item)){
+        newArray.push(item);
+      }
+    })
+    return newArray;
+  }
+
+
+// store the index of the items to be removed in new array. 
+// loop the splice method to remove it for each element in the new array. 
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
