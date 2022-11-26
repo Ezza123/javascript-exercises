86f44708987140341477738a071affe66469d37a
Ezza123 <p.balkowski123@gmail.com>
2022-11-26 14:27:59 +0100

    sumall_solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(...arg) {
 
-};
+  let sum = 0;
+
+  for (i = 0; i < Math.abs(arg[0] - arg[1]) + 1; ++i){
+    if(arg[0] > arg[1]){
+      sum += (arg[1] + i);  
+       
+    } else if ( arg[0] < arg[1]){
+      sum += (arg[0] + i);         
+    } 
+  }
+  
+  if (typeof (arg[0] + arg[1]) !== 'number' || sum < 0) {
+    return "ERROR"
+    
+  } else {
+    return sum;
+  }
+}
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
